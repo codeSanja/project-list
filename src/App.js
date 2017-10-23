249415e322f60bfd64da8ffe308c5dcaf9add425
mandics <sanja.mandic@hmhco.com>
2017-10-23 17:33:34 +0100

    Listing projects.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ import Projects from "./Components/Projects"
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+          projects: [
+              {
+                  title: "Business Website",
+                  category: "Web Design"
+              },
+              {
+                  title: "Social App",
+                  category: "Mobile Development"
+              },
+              {
+                  title: "Ecommerce Shopping Cart",
+                  category: "Web Development"
+              }
+          ]
+      }
+    }
+
   render() {
     return (
       <div className="App">
         MY APP
-        <Projects test="Hello World!"/>
+        <Projects projects={this.state.projects}/>
       </div>
     );
   }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
+import ProjectItem from './ProjectItem';
 
 class Projects extends Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    var projectItems = [];
+    if (this.props.projects) {
+      projectItems = this.props.projects.map(project => {
+        console.log(project);
+        return <ProjectItem project={project} />;
+      });
+    } else {
+      projectItems = [];
     }
 
-    render() {
-        return (
-            <div className="Projects">
-                My Projects {this.props.test}
-            </div>
-        );
-    }
+    return (
+      <div className="Projects" projects={this.props.projects}>
+          {projectItems}
+      </div>
+    );
+  }
 }
 
 export default Projects;
